1cbfaa3eee7e2d42ec90f83a3415a57c98b31a60
alvidi12 <an.villarroeld@duocuc.cl>
2025-11-08 13:43:11 -0300

    frontend

---
+++ src/components/PersonaList.js
@@ -0,0 +1,34 @@
+import React, {useEffect, useState} from "react";
+import axios from "axios";
+import PersonasForm from "./PersonaForm";
+
+const API_URL= "https://backend-fs-v-r6gs.onrender.com/api/personas"
+
+function PersonaList(){
+    const [personas, setPersonas] = useState([]);
+    const cargar = async () =>{
+        const res = await axios.get(API_URL);
+        setPersonas(res.data);
+    };
+
+    const eliminar = async (id)=>{
+        await axios.delete(`${API_URL}/${id}`);
+        cargar();
+    };
+
+    useEffect(() =>{
+        cargar();
+    }, []);
+
+    return (
+        <ul className="list-group">
+            {pesonas.map(p =>(
+                <li key ={p.id} className="list-group-item d-flex justify-content-between">
+                    {p.nombre}
+                    <button className="btn btn-danger btn-sm" onClick={()=>eliminar(p.id)}>Eliminar</button>
+                </li>
+            ))}
+        </ul>
+    );
+}
+export default PersonaList;
